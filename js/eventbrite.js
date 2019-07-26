d5a3be29e7611e2194ce3ce68f38a195417fefe2
Karlafgl <karlafgl@gmail.com>
2019-07-25 19:34:09 -0600

    getting categories with api

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -7,6 +7,14 @@ class EventBrite {
   //Obtener categorias desde el init 
   async obtenerCategorias(){
     //Consultar las categorias a la REST API
-     
+     const respuestaCategorias = await fetch(`https://www.eventbriteapi.com/v3/categories/?token=${this.token_auth}`)
+
+     //Esperar la respuesta de las categorias y devolver un json 
+     const categorias = await respuestaCategorias.json()
+
+     //Devolvemos el resultado
+     return{
+       categorias
+     }
   }
 }
