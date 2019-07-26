4174619969c31d551b4d24666bb943b8069ba65b
Karlafgl <karlafgl@gmail.com>
2019-07-25 19:47:57 -0600

    reading the categories on events

--- js/interfaz.js
+++ js/interfaz.js
@@ -9,11 +9,26 @@ class Interfaz {
 
   //Metodo para cuando inicialize la app
   init() {
-
+    //llamar a imprimir categorias de la Rest API
+    this.imprimirCategorias()
   }
 
   //Imprimir categorias
   imprimirCategorias(){
     const listadoCategorias = eventBrite.obtenerCategorias()
+      .then(categorias => {
+        const cats = categorias.categorias.categories;
+        console.log(cats)
+        //seleccionar el select de categorias
+        const selectCategoria = document.getElementById('listado-categorias')
+
+        //Recorremos el arreglo e imprimos los <options></options>
+        cats.forEach(cat => {
+          const option = document.createElement('option')
+          option.value = cat.id
+          option.appendChild(document.createTextNode(cat.name_localized))
+          selectCategoria.appendChild(option)
+        });
+      })
   }
 }
