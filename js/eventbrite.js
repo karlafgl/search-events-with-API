0e0cb6fe647c2ee11d2cc1864e94c308cdd7b90e
Karlafgl <karlafgl@gmail.com>
2019-07-29 12:43:50 -0600

    adding new method to the ui

--- js/eventbrite.js
+++ js/eventbrite.js
@@ -17,4 +17,15 @@ class EventBrite {
        categorias
      }
   }
+
+  async obtenerEventos(evento, categoria){
+    const respuestaEvento = await fetch(`https://www.eventbriteapi.com/v3/events/search/?q=${evento}&sort_by=${this.ordenar}&categories=${categoria}&token=${this.token_auth}`)
+    
+    const eventos = await respuestaEvento.json()
+    console.log(eventos)
+    return {
+      eventos
+    }
+  }
+
 }
--- js/interfaz.js
+++ js/interfaz.js
@@ -18,7 +18,7 @@ class Interfaz {
     const listadoCategorias = eventBrite.obtenerCategorias()
       .then(categorias => {
         const cats = categorias.categorias.categories;
-        console.log(cats)
+
         //seleccionar el select de categorias
         const selectCategoria = document.getElementById('listado-categorias')
 
@@ -31,4 +31,53 @@ class Interfaz {
         });
       })
   }
+
+  limpiarMensaje(){
+    const alert = document.querySelector('.alert')
+    if(alert){
+      alert.remove()
+    }
+  }
+  //metodo para imprimir mensajes tomara 2 parametros
+  mostrarMensaje(mensaje, clase){
+    const div = document.createElement('div')
+    div.appendChild(document.createTextNode(mensaje))
+    div.classList = clase
+    const buscadorDiv = document.getElementById('resultado-eventos')
+    buscadorDiv.appendChild(div)
+    setTimeout(() => { 
+      this.limpiarMensaje() 
+    }, 3000);
+  }
+
+ // Lee la respuesta de la API e imprime los resultados
+ mostrarEventos(eventos) {
+  // leer los eventos y agregarlos a una variable
+  const listaEventos = eventos.events;
+
+  // recorrer los eventos y crear su template
+  listaEventos.forEach(evento => {
+       this.listado.innerHTML += `
+            <div class="col-md-4 mb-4">
+                 <div class="card">
+                      <img class="img-fluid mb-2" src="${evento.logo !== null ? evento.logo.url : ''}"> 
+                      <div class="card-body">
+                           <div class="card-text">
+                                <h2 class="text-center">${evento.name.text}</h2>
+                                <p class="lead text-info">Información del evento</p>
+                                <p>${evento.description.text.substring(0,280)}...</p>
+
+                                <span class="badge badge-primary">Capacidad: ${evento.capacity}</span>
+                                <span class="badge badge-secondary">Fecha y hora: ${evento.start.local}</span>
+
+                                <a href="${evento.url}" target="_blank" class="btn btn-primary btn-block mt-4">Comprar Boletos</a>  
+                           </div>
+                      </div>
+
+                 </div>
+            </div>
+       `;
+  })
+}
+
 }
